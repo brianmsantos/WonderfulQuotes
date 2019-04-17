f3f8dbc952f068ff22e752813a8e8bc8e05fef01
Brian Santos <brianmsantos81@gmail.com>
2019-04-17 16:49:14 -0700

    event handler corrected

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
     <div class="container">
-    
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './Components/QuoteGrid.vue';
+    import NewQuote from './Components/NewQuote.vue';
+
     export default {
-        
+        data: function(){
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote){
+            this.quotes.push(quote)
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        }
     }
 </script>
 
